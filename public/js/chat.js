7617671940890e99750bf2b3cecb2cc395008d6c
Dan Triano <dan.triano@gmail.com>
2021-03-22 17:01:54 +0100

    Enunciado

---
+++ public/js/chat.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  //Inicializa socket con IO
+  //const socket =
+
+  $("#send").submit(function (e) {
+    e.preventDefault();
+    var msg = $("#msg").val();
+    //Acciones a realizar cuando se pulsa el boton submit
+   
+  });
+
+  //We listen on "newMsg" and react if we detect 
+  //Acciones a realizar cuando se detecta actividad en el canal newMsg
+
+});
